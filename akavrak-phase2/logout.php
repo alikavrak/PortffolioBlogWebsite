148309dc26af6b9b664e2d2c977e3df202c63848
alikavrak <aliikavrak@yahoo.com>
2026-03-09 23:49:05 +0000

    Delete akavrak-phase2 directory

--- akavrak-phase2/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();          // Oturumu başlat
-session_unset();          // Tüm session değişkenlerini temizle
-session_destroy();        // Oturumu sonlandır
-
-// Login sayfasına yönlendir
-header("Location: login.php");
-exit();
-?>
